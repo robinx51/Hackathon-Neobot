bba48ec1d539e02d69e01b3d947fa0129e34f6a2
cnrprod1725372212-user-89366 <n0vest@yandex.ru>
2025-01-25 16:44:33 +0300

    Изменены длины сиволов в соответствии с требованиями

--- DataEngeeners/DB/Tables/users.sql
+++ DataEngeeners/DB/Tables/users.sql
@@ -4,9 +4,9 @@ CREATE TABLE users (
     user_id INTEGER PRIMARY KEY, 
     telegram_id BIGINT NOT NULL,
     role VARCHAR(10) DEFAULT 'visitor',
-    first_name VARCHAR(50) NULL,
-    last_name VARCHAR(50) NULL,
-    email VARCHAR(40) NULL,
-    city VARCHAR(40) NULL,
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
+    email VARCHAR(64) NULL,
+    city VARCHAR(35) NULL,
     phone_number CHAR(12) NULL
 )
