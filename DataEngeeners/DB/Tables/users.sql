5ef0f7dffdad94d28d642c3c850e4b48c843375e
cnrprod1725372212-user-89366 <n0vest@yandex.ru>
2025-01-25 17:20:26 +0300

    Исправил тип для role

--- DataEngeeners/DB/Tables/users.sql
+++ DataEngeeners/DB/Tables/users.sql
@@ -3,7 +3,7 @@
 CREATE TABLE users (
     user_id INTEGER PRIMARY KEY, 
     telegram_id BIGINT NOT NULL,
-    role VARCHAR(10) DEFAULT 'visitor',
+    role VARCHAR(20) DEFAULT 'visitor',
     first_name VARCHAR(30) NULL,
     last_name VARCHAR(30) NULL,
     email VARCHAR(64) NULL,
